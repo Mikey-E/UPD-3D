a8e014e722063339cb5e34d9644563bae7b89ff1
Michael E. Elgin <melgin@uwyo.edu>
2025-06-18 15:58:29 -0600

    utils/slurm_minigpt-3d_prog.sh removed old argument check that no longer applies

--- utils/slurm_minigpt-3d_prog.sh
+++ utils/slurm_minigpt-3d_prog.sh
@@ -11,11 +11,6 @@
 #SBATCH --mem=16G
 #SBATCH --time=01-23:59:59
 
-if [ "$#" -ne 1 ]; then
-    echo "Usage: sbatch run_minigpt3d.slurm <folder_name>"
-    exit 1
-fi
-
 # Load the necessary modules - this may not work, the conda environment may have to be activated in the command line before you run this script
 conda init
 conda activate minigpt_3d
