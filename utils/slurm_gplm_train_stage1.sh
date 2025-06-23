16164a9d59309b1f7dee258b0e7398f066461bfa
Michael E. Elgin <melgin@uwyo.edu>
2025-06-23 12:21:53 -0600

    gplm training slurm files

---
+++ utils/slurm_gplm_train_stage1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --account=3dllms
+##SBATCH --partition=mb-l40s
+#SBATCH --partition=mb-a30
+#SBATCH --job-name=gplm_train_stage1
+#SBATCH --output=gplm_train_stage1_%j.out
+#SBATCH --error=gplm_train_stage1_%j.out
+#SBATCH --gres=gpu:1
+#SBATCH --mem=64G
+#SBATCH --time=7-00:00:00
+
+export WANDB_MODE=disabled
+bash ./release/paper/scripts/train/1.sh
