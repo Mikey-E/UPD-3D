191ceaae6640a22b881ea7a84a0f2a6104731f8f
Michael E. Elgin <melgin@uwyo.edu>
2025-06-25 11:15:51 -0600

    mgpt3d slurm files for combined data training

---
+++ utils/slurm_mgpt3d_train_stage2_combined.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --account=3dllms
+#SBATCH --partition=mb-a30
+#SBATCH --job-name=mgpt3d_train_stage2
+#SBATCH --output=mgpt3d_train_stage2_%j.out
+#SBATCH --error=mgpt3d_train_stage2_%j.out
+#SBATCH --gres=gpu:1
+#SBATCH --mem=16G
+#SBATCH --time=24:00:00
+
+export MASTER_ADDR=localhost
+export MASTER_PORT=$(python -c "import socket; s=socket.socket(); s.bind(('',0)); print(s.getsockname()[1]); s.close()") #Determines a free port and assigns it
+export WANDB_MODE=disabled #Otherwise it will give and error for trying to log to wandb
+CUDA_VISIBLE_DEVICES=0 python train.py --cfg-path ./train_configs/MiniGPT_3D/stage_2_combined.yaml
