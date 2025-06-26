cbee68b7f1671031f92f3f1a3267c414733a4735
Michael E. Elgin <melgin@uwyo.edu>
2025-06-25 18:18:42 -0600

    bug/typo fix in slurm files for mgpt3d combined training

--- utils/slurm_mgpt3d_train_stage2_combined.sh
+++ utils/slurm_mgpt3d_train_stage2_combined.sh
@@ -11,4 +11,4 @@
 export MASTER_ADDR=localhost
 export MASTER_PORT=$(python -c "import socket; s=socket.socket(); s.bind(('',0)); print(s.getsockname()[1]); s.close()") #Determines a free port and assigns it
 export WANDB_MODE=disabled #Otherwise it will give and error for trying to log to wandb
-CUDA_VISIBLE_DEVICES=0 python train.py --cfg-path ./train_configs/MiniGPT_3D/mgpt3d_stage_2_combined.yaml
+CUDA_VISIBLE_DEVICES=0 python train.py --cfg-path ./train_configs/MiniGPT_3D/mgpt3d_stage2_combined.yaml
