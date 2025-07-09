6fe2d51d12180cd51649fef8571bdee62629edb0
Michael E. Elgin <melgin@uwyo.edu>
2025-07-08 21:34:07 -0600

    utils/mgpt3d_format_upd_text.py caption flag is set in filename

--- utils/mgpt3d_format_upd_text.py
+++ utils/mgpt3d_format_upd_text.py
@@ -96,7 +96,9 @@ if __name__ == "__main__":
                     all_objects.append(parse_txt_file(file_path, is_standard_answer, caption_folder))
         output_file = os.path.join(
             os.path.dirname(__file__),
-            f"mgpt3d_format_overall_{pcl_list_tag if pcl_list_tag else os.path.basename(os.path.abspath(overall_dir))}.json"
+            f"mgpt3d_format_overall_{pcl_list_tag if pcl_list_tag else os.path.basename(os.path.abspath(overall_dir))}"
+            + (f"_captions" if caption_folder else "")
+            + ".json"
         )
         with open(output_file, 'w', encoding='utf-8') as f:
             json.dump(all_objects, f, indent=2)
@@ -105,7 +107,9 @@ if __name__ == "__main__":
         input_dir = args.input_dir
         output_file = os.path.join(
             os.path.dirname(__file__),
-            f"mgpt3d_format_{pcl_list_tag if pcl_list_tag else os.path.basename(os.path.abspath(input_dir))}.json"
+            f"mgpt3d_format_{pcl_list_tag if pcl_list_tag else os.path.basename(os.path.abspath(input_dir))}"
+            + (f"_captions" if caption_folder else "")
+            + ".json"
         )
         process_directory(input_dir, output_file, pcl_set, caption_folder)
         print(f"Output written to {output_file}")
