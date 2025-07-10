d461405edd806d2cc5f19874fab2fed9ff51c05a
Michael E. Elgin <melgin@uwyo.edu>
2025-07-09 19:29:59 -0600

    slurm_score_model_responses.sh job name, output files, memory, time limit

--- slurm_score_model_responses.sh
+++ slurm_score_model_responses.sh
@@ -3,8 +3,12 @@
 #Example use: sbatch slurm_score_model_responses.sh args/flags...
 
 #SBATCH --account=3dllms
-#SBATCH --time=23:00:00
 #SBATCH --partition=non-investor
+#SBATCH --job-name=score_model_responses
+#SBATCH --output=score_model_responses_%j.out
+#SBATCH --error=score_model_responses_%j.out
+#SBATCH --mem=16G
+#SBATCH --time=7-00:00:00
 
 # Check if OPENAI_API_KEY is set
 if [ -z "$OPENAI_API_KEY" ]; then
