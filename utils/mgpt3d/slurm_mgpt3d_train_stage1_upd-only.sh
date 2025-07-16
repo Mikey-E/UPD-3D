7fec868c43319d769caa93f4cab99b7f4f17b4aa
Michael E. Elgin <melgin@uwyo.edu>
2025-07-16 13:00:24 -0600

    slurm files for mgpt3d training on only upd-data

---
+++ utils/mgpt3d/slurm_mgpt3d_train_stage1_upd-only.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --account=3dllms
+#SBATCH --partition=mb-a30
+#SBATCH --job-name=mgpt3d_train_stage1_upd-only
+#SBATCH --output=mgpt3d_train_stage1_upd-only_%j.out
+#SBATCH --error=mgpt3d_train_stage1_upd-only_%j.out
+#SBATCH --gres=gpu:1
+#SBATCH --mem=16G
+#SBATCH --time=7-00:00:00
+
+#This ensures conda activate works in non-interactive shells.
+#running conda init every time won't work. Just make sure to source the correct conda.sh
+source /project/3dllms/melgin/conda/etc/profile.d/conda.sh
+
+#Activate the conda environment just in case you didn't already in the command line.
+conda activate minigpt-3d
+
+export MASTER_ADDR=localhost
+export MASTER_PORT=$(python -c "import socket; s=socket.socket(); s.bind(('',0)); print(s.getsockname()[1]); s.close()") #Determines a free port and assigns it
+export WANDB_MODE=disabled #Otherwise it will give and error for trying to log to wandb
+CUDA_VISIBLE_DEVICES=0 python train.py --cfg-path ./train_configs/MiniGPT_3D/mgpt3d_stage1_upd-only.yaml
