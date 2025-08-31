1656b915c231653086686af0f5b1a016235f6bf4
Michael E. Elgin <melgin@uwyo.edu>
2025-08-30 20:39:23 -0600

    utils/mgpt3d/multi-slurm_mgpt3d_inf.sh updated with more recent vars and positioning

--- utils/mgpt3d/multi-slurm_mgpt3d_inf.sh
+++ utils/mgpt3d/multi-slurm_mgpt3d_inf.sh
@@ -15,10 +15,11 @@ subfolders=(
 
 for subfolder in "${subfolders[@]}"; do
     sbatch slurm_mgpt3d_inf.sh \
-    --cfg-path ./eval_configs/mgpt3d_eval-config_ft-comb.yaml \
+    --cfg-path ./eval_configs/MiniGPT_3D_conv_UI_demo.yaml \
     --upd_text_folder_path /project/3dllms/melgin/UPD-3D/upd_text \
-    --unzipped_point_cloud_path /gscratch/melgin/3d-grand_unzipped/3D-FRONT \
-    --pcl_list_txt_file_path /project/3dllms/melgin/UPD-3D/pcl_lists/3D-FRONT_test.txt \
-    --json_tag ft-comb \
-    --upd_version_name_subfolder "${subfolder}"
+    --upd_version_name "Crops3D_gpt-5-nano" \
+    --upd_version_name_subfolder "${subfolder}" \
+    --unzipped_point_cloud_path /gscratch/melgin/CEA/Crops3D \
+    --pcl_list_txt_file_path /project/3dllms/melgin/UPD-3D/pcl_lists/Crops3D_test.txt \
+    --json_tag ft-comb
 done
