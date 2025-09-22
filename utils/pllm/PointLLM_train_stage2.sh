c7c12fece8813e9ca884357dc8ba36a7d3227cdc
Michael E. Elgin <melgin@uwyo.edu>
2025-09-22 13:44:35 -0600

    utils/pllm/PointLLM_train_stage2.sh in state for Crops3D

--- utils/pllm/PointLLM_train_stage2.sh
+++ utils/pllm/PointLLM_train_stage2.sh
@@ -1,18 +1,26 @@
+#scripts/PointLLM_train_stage2.sh
+
 master_port=$((RANDOM % (65535 - 49152 + 1) + 49152))
 # Get the filename without extension
 filename=$(basename "$0" | cut -f 1 -d '.')
 
-dir_path=PointLLM
+dir_path=PointLLM_ft-comb
 
-model_name_or_path=outputs/PointLLM_train_stage1/PointLLM_train_stage1 # Path to the output dir of stage 1 training
-data_path=data/objaverse_data
-anno_path=data/anno_data/PointLLM_complex_instruction_70K.json
-output_dir=outputs/PointLLM_train_stage2/$filename
+model_name_or_path=outputs/PointLLM_train_stage1_ft-comb_Crops3D_gpt-5-nano/PointLLM_train_stage1 # Path to the output dir of stage 1 training
+
+# data_path=data/objaverse_data
+data_path=data_ft-comb_Crops3D_gpt-5-nano/objaverse_data
+
+# anno_path=data/anno_data/PointLLM_complex_instruction_70K.json
+anno_path=data_ft-comb_Crops3D_gpt-5-nano/anno_data/PointLLM_brief_description_660K_filtered.json
+
+# output_dir=outputs/PointLLM_train_stage2_3D-FRONT_attempt2/$filename
+output_dir=outputs/PointLLM_train_stage2_ft-comb_Crops3D_gpt-5-nano/$filename
 
 cd $dir_path
 
 # --report_to wandb \
-# --nproc_per_node 8 \
+# --nproc_per_node 8
 PYTHONPATH=$dir_path:$PYTHONPATH \
 torchrun --nnodes=1 --nproc_per_node=8 --master_port=$master_port pointllm/train/train_mem.py \
     --model_name_or_path $model_name_or_path \
