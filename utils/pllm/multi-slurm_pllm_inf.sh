f097c23d892336de0d4ac4e64d67b23aadd98124
Michael E. Elgin <melgin@uwyo.edu>
2025-09-22 13:45:52 -0600

    utils/pllm/multi-slurm_pllm_inf.sh in state for Crops3D

--- utils/pllm/multi-slurm_pllm_inf.sh
+++ utils/pllm/multi-slurm_pllm_inf.sh
@@ -15,11 +15,11 @@ subfolders=(
 
 for subfolder in "${subfolders[@]}"; do
     sbatch slurm_pllm_inf.sh \
-        --model_name ./outputs/PointLLM_train_stage2/PointLLM_train_stage2/ \
+        --model_name ./outputs/PointLLM_train_stage2_ft-comb_Crops3D_gpt-5-nano/PointLLM_train_stage2/ \
         --upd_text_folder_path /project/3dllms/melgin/UPD-3D/upd_text/ \
         --unzipped_point_cloud_path /gscratch/melgin/CEA/Crops3D/ \
         --pcl_list_txt_file_path /project/3dllms/melgin/UPD-3D/pcl_lists/Crops3D_test.txt \
-        --json_tag base \
+        --json_tag ft-comb \
         --upd_version_name_subfolder "$subfolder" \
         --upd_version_name "Crops3D_gpt-5-nano"
 done
