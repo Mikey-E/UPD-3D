fdf7e5e2da49d09f18cf01a4a655d52a0bf50843
Michael E. Elgin <melgin@uwyo.edu>
2025-09-22 14:03:45 -0600

    utils/shpllm/multi-slurm_shpllm_inf.sh has codeblocks for different runs of different models/datasets

--- utils/shpllm/multi-slurm_shpllm_inf.sh
+++ utils/shpllm/multi-slurm_shpllm_inf.sh
@@ -13,22 +13,48 @@ subfolders=(
     "ivqd_additional_instruction"
 )
 
-for subfolder in "${subfolders[@]}"; do
-    sbatch slurm_shpllm_inf.sh \
-    --model_path checkpoints/shapellm-13b-general-v1.0-finetune \
-    --upd_text_folder_path /project/3dllms/melgin/UPD-3D/upd_text \
-    --unzipped_point_cloud_path /gscratch/melgin/3d-grand_unzipped/3D-FRONT \
-    --pcl_list_txt_file_path /project/3dllms/melgin/UPD-3D/pcl_lists/3D-FRONT_test.txt \
-    --json_tag ft-cap3d \
-    --upd_version_name_subfolder "${subfolder}"
-done
+#inference for base (cap3d) model on 3D-FRONT
+# for subfolder in "${subfolders[@]}"; do
+#     sbatch slurm_shpllm_inf.sh \
+#     --model_path checkpoints/shapellm-13b-general-v1.0-finetune \
+#     --upd_text_folder_path /project/3dllms/melgin/UPD-3D/upd_text \
+#     --unzipped_point_cloud_path /gscratch/melgin/3d-grand_unzipped/3D-FRONT \
+#     --pcl_list_txt_file_path /project/3dllms/melgin/UPD-3D/pcl_lists/3D-FRONT_test.txt \
+#     --json_tag ft-cap3d \
+#     --upd_version_name_subfolder "${subfolder}"
+# done
 
+#inference for base (cap3d) model on Crops3D
+# for subfolder in "${subfolders[@]}"; do
+#     sbatch slurm_shpllm_inf.sh \
+#     --model_path checkpoints/shapellm-13b-general-v1.0-finetune \
+#     --upd_text_folder_path /project/3dllms/melgin/UPD-3D/upd_text \
+#     --unzipped_point_cloud_path /gscratch/melgin/CEA/Crops3D \
+#     --pcl_list_txt_file_path /project/3dllms/melgin/UPD-3D/pcl_lists/Crops3D_test.txt \
+#     --json_tag ft-cap3d \
+#     --upd_version_name_subfolder "${subfolder}" \
+#     --upd_version_name "Crops3D_gpt-5-nano"
+# done
+
+#inference for upd finetuned model on 3D-FRONT
+# for subfolder in "${subfolders[@]}"; do
+#     sbatch slurm_shpllm_inf.sh \
+#     --model_path checkpoints/shapellm-13b-general-v1.0-finetune-upd \
+#     --upd_text_folder_path /project/3dllms/melgin/UPD-3D/upd_text \
+#     --unzipped_point_cloud_path /gscratch/melgin/3d-grand_unzipped/3D-FRONT \
+#     --pcl_list_txt_file_path /project/3dllms/melgin/UPD-3D/pcl_lists/3D-FRONT_test.txt \
+#     --json_tag ft-upd \
+#     --upd_version_name_subfolder "${subfolder}"
+# done
+
+#inference for upd finetuned model on Crops3D
 for subfolder in "${subfolders[@]}"; do
     sbatch slurm_shpllm_inf.sh \
-    --model_path checkpoints/shapellm-13b-general-v1.0-finetune-upd \
+    --model_path checkpoints/shapellm-13b-general-v1.0-finetune-upd_Crops3D_gpt-5-nano \
     --upd_text_folder_path /project/3dllms/melgin/UPD-3D/upd_text \
-    --unzipped_point_cloud_path /gscratch/melgin/3d-grand_unzipped/3D-FRONT \
-    --pcl_list_txt_file_path /project/3dllms/melgin/UPD-3D/pcl_lists/3D-FRONT_test.txt \
+    --unzipped_point_cloud_path /gscratch/melgin/CEA/Crops3D \
+    --pcl_list_txt_file_path /project/3dllms/melgin/UPD-3D/pcl_lists/Crops3D_test.txt \
     --json_tag ft-upd \
-    --upd_version_name_subfolder "${subfolder}"
+    --upd_version_name_subfolder "${subfolder}" \
+    --upd_version_name "Crops3D_gpt-5-nano"
 done
