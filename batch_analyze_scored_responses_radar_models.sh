954d87ae7e45182ed08c8a56ff791d279ff57721
Michael E. Elgin <melgin@uwyo.edu>
2025-10-07 10:58:41 -0600

    multi-modal radar charts for Crops3D

--- batch_analyze_scored_responses_radar_models.sh
+++ batch_analyze_scored_responses_radar_models.sh
@@ -70,70 +70,122 @@ format_title() {
     esac
 }
 
-# Loop through each category
-for category in "${categories[@]}"; do
-    echo "Processing category: $category"
+# Array of datasets to process
+datasets=("3D-FRONT" "Crops3D_gpt-5-nano")
+
+# Loop through each dataset
+for dataset in "${datasets[@]}"; do
+    echo "========================================"
+    echo "Processing dataset: $dataset"
+    echo "========================================"
     
-    # Format the title
-    formatted_title=$(format_title "$category")
-    
-    # Set output folder name based on dual mode
-    if [[ -n "$DUAL_FLAG" ]]; then
-        formatted_title="$formatted_title Dual Accuracy"
-        output_folder="UPD-3D-FRONT_Model_Performance_Comparison_Dual"
-    else
-        output_folder="UPD-3D-FRONT_Model_Performance_Comparison"
-    fi
-    
-    # Build file lists for current category
-    base_model_files=(
-        "./scored_model_responses/3D-FRONT_test_mgpt3d/inf_rslts_mgpt3d_3D-FRONT_test_${category}_scored.json"
-        "scored_model_responses/3D-FRONT_test_gplm/inf_rslts_gplm_3D-FRONT_test_${category}_scored.json"
-        "scored_model_responses/3D-FRONT_test_pllm/inf_rslts_pllm_base_3D-FRONT_test_${category}_scored.json"
-        "scored_model_responses/3D-FRONT_test_shpllm_ft-cap3d/inf_rslts_shapellm-13b-general-v1.0-finetune_ft-cap3d_3D-FRONT_test_${category}_scored.json"
-        "scored_model_responses/3D-FRONT_test_llava3d_base/inf_rslts_llava3d_base_3D-FRONT_test_${category}_scored.json"
-    )
-    
-    trained_model_files=(
-        "./scored_model_responses/3D-FRONT_test_mgpt3d_ft-comb/inf_rslts_mgpt3d_ft-comb_3D-FRONT_test_${category}_scored.json"
-        "scored_model_responses/3D-FRONT_test_gplm_ft-comb/inf_rslts_gplm_ft-comb_3D-FRONT_test_${category}_scored.json"
-        "scored_model_responses/3D-FRONT_test_pllm_ft-comb/inf_rslts_pllm_ft-comb_3D-FRONT_test_${category}_scored.json"
-        "scored_model_responses/3D-FRONT_test_shpllm_ft-upd/inf_rslts_shapellm-13b-general-v1.0-finetune-upd_ft-upd_3D-FRONT_test_${category}_scored.json"
-        "scored_model_responses/3D-FRONT_test_llava3d_ft-upd_3D-FRONT/inf_rslts_llava3d_ft-upd_3D-FRONT_3D-FRONT_test_${category}_scored.json"
-    )
-    
-    # If in dual mode and not processing standard/open_ended, add standard files to each series
-    if [[ -n "$DUAL_FLAG" && "$category" != "standard" && "$category" != "open_ended"* ]]; then
-        # Add corresponding standard files to each series
-        base_model_files+=(
-            "./scored_model_responses/3D-FRONT_test_mgpt3d/inf_rslts_mgpt3d_3D-FRONT_test_standard_scored.json"
-            "scored_model_responses/3D-FRONT_test_gplm/inf_rslts_gplm_3D-FRONT_test_standard_scored.json"
-            "scored_model_responses/3D-FRONT_test_pllm/inf_rslts_pllm_base_3D-FRONT_test_standard_scored.json"
-            "scored_model_responses/3D-FRONT_test_shpllm_ft-cap3d/inf_rslts_shapellm-13b-general-v1.0-finetune_ft-cap3d_3D-FRONT_test_standard_scored.json"
-            "scored_model_responses/3D-FRONT_test_llava3d_base/inf_rslts_llava3d_base_3D-FRONT_test_standard_scored.json"
-        )
+    # Loop through each category
+    for category in "${categories[@]}"; do
+        echo "Processing category: $category for $dataset"
         
-        trained_model_files+=(
-            "./scored_model_responses/3D-FRONT_test_mgpt3d_ft-comb/inf_rslts_mgpt3d_ft-comb_3D-FRONT_test_standard_scored.json"
-            "scored_model_responses/3D-FRONT_test_gplm_ft-comb/inf_rslts_gplm_ft-comb_3D-FRONT_test_standard_scored.json"
-            "scored_model_responses/3D-FRONT_test_pllm_ft-comb/inf_rslts_pllm_ft-comb_3D-FRONT_test_standard_scored.json"
-            "scored_model_responses/3D-FRONT_test_shpllm_ft-upd/inf_rslts_shapellm-13b-general-v1.0-finetune-upd_ft-upd_3D-FRONT_test_standard_scored.json"
-            "scored_model_responses/3D-FRONT_test_llava3d_ft-upd_3D-FRONT/inf_rslts_llava3d_ft-upd_3D-FRONT_3D-FRONT_test_standard_scored.json"
-        )
-    fi
+        # Format the title
+        formatted_title=$(format_title "$category")
+        
+        # Set output folder name based on dual mode and dataset
+        if [[ -n "$DUAL_FLAG" ]]; then
+            formatted_title="$formatted_title Dual Accuracy"
+            output_folder="UPD-${dataset}_Model_Performance_Comparison_Dual"
+        else
+            output_folder="UPD-${dataset}_Model_Performance_Comparison"
+        fi
+        
+        # Build file lists for current category based on dataset
+        if [[ "$dataset" == "3D-FRONT" ]]; then
+            # 3D-FRONT file patterns
+            base_model_files=(
+                "./scored_model_responses/3D-FRONT_test_mgpt3d/inf_rslts_mgpt3d_3D-FRONT_test_${category}_scored.json"
+                "scored_model_responses/3D-FRONT_test_gplm/inf_rslts_gplm_3D-FRONT_test_${category}_scored.json"
+                "scored_model_responses/3D-FRONT_test_pllm/inf_rslts_pllm_base_3D-FRONT_test_${category}_scored.json"
+                "scored_model_responses/3D-FRONT_test_shpllm_ft-cap3d/inf_rslts_shapellm-13b-general-v1.0-finetune_ft-cap3d_3D-FRONT_test_${category}_scored.json"
+                "scored_model_responses/3D-FRONT_test_llava3d_base/inf_rslts_llava3d_base_3D-FRONT_test_${category}_scored.json"
+            )
+            
+            trained_model_files=(
+                "./scored_model_responses/3D-FRONT_test_mgpt3d_ft-comb/inf_rslts_mgpt3d_ft-comb_3D-FRONT_test_${category}_scored.json"
+                "scored_model_responses/3D-FRONT_test_gplm_ft-comb/inf_rslts_gplm_ft-comb_3D-FRONT_test_${category}_scored.json"
+                "scored_model_responses/3D-FRONT_test_pllm_ft-comb/inf_rslts_pllm_ft-comb_3D-FRONT_test_${category}_scored.json"
+                "scored_model_responses/3D-FRONT_test_shpllm_ft-upd/inf_rslts_shapellm-13b-general-v1.0-finetune-upd_ft-upd_3D-FRONT_test_${category}_scored.json"
+                "scored_model_responses/3D-FRONT_test_llava3d_ft-upd_3D-FRONT/inf_rslts_llava3d_ft-upd_3D-FRONT_3D-FRONT_test_${category}_scored.json"
+            )
+        else
+            # Crops3D_gpt-5-nano file patterns
+            base_model_files=(
+                "./scored_model_responses/Crops3D_gpt-5-nano_test_mgpt3d_base/inf_rslts_mgpt3d_base_Crops3D_test_${category}_scored.json"
+                "scored_model_responses/Crops3D_gpt-5-nano_test_gplm_base/inf_rslts_gplm_base_Crops3D_test_${category}_scored.json"
+                "scored_model_responses/Crops3D_gpt-5-nano_test_pllm_base/inf_rslts_pllm_base_Crops3D_test_${category}_scored.json"
+                "scored_model_responses/Crops3D_gpt-5-nano_test_shpllm_ft-cap3d/inf_rslts_shapellm-13b-general-v1.0-finetune_ft-cap3d_Crops3D_test_${category}_scored.json"
+                "scored_model_responses/Crops3D_gpt-5-nano_test_llava3d_base/inf_rslts_llava3d_base_Crops3D_test_${category}_scored.json"
+            )
+            
+            trained_model_files=(
+                "./scored_model_responses/Crops3D_gpt-5-nano_test_mgpt3d_ft-comb/inf_rslts_mgpt3d_ft-comb_Crops3D_test_${category}_scored.json"
+                "scored_model_responses/Crops3D_gpt-5-nano_test_gplm_ft-comb/inf_rslts_gplm_ft-comb_Crops3D_test_${category}_scored.json"
+                "scored_model_responses/Crops3D_gpt-5-nano_test_pllm_ft-comb/inf_rslts_pllm_ft-comb_Crops3D_test_${category}_scored.json"
+                "scored_model_responses/Crops3D_gpt-5-nano_test_shpllm_ft-upd/inf_rslts_shapellm-13b-general-v1.0-finetune-upd_Crops3D_gpt-5-nano_ft-upd_Crops3D_test_${category}_scored.json"
+                "scored_model_responses/Crops3D_gpt-5-nano_test_llava3d_ft-upd_Crops3D_gpt-5-nano/inf_rslts_llava3d_ft-upd_Crops3D_gpt-5-nano_Crops3D_test_${category}_scored.json"
+            )
+        fi
+        
+        # If in dual mode and not processing standard/open_ended, add standard files to each series
+        if [[ -n "$DUAL_FLAG" && "$category" != "standard" && "$category" != "open_ended"* ]]; then
+            # Add corresponding standard files to each series
+            if [[ "$dataset" == "3D-FRONT" ]]; then
+                base_model_files+=(
+                    "./scored_model_responses/3D-FRONT_test_mgpt3d/inf_rslts_mgpt3d_3D-FRONT_test_standard_scored.json"
+                    "scored_model_responses/3D-FRONT_test_gplm/inf_rslts_gplm_3D-FRONT_test_standard_scored.json"
+                    "scored_model_responses/3D-FRONT_test_pllm/inf_rslts_pllm_base_3D-FRONT_test_standard_scored.json"
+                    "scored_model_responses/3D-FRONT_test_shpllm_ft-cap3d/inf_rslts_shapellm-13b-general-v1.0-finetune_ft-cap3d_3D-FRONT_test_standard_scored.json"
+                    "scored_model_responses/3D-FRONT_test_llava3d_base/inf_rslts_llava3d_base_3D-FRONT_test_standard_scored.json"
+                )
+                
+                trained_model_files+=(
+                    "./scored_model_responses/3D-FRONT_test_mgpt3d_ft-comb/inf_rslts_mgpt3d_ft-comb_3D-FRONT_test_standard_scored.json"
+                    "scored_model_responses/3D-FRONT_test_gplm_ft-comb/inf_rslts_gplm_ft-comb_3D-FRONT_test_standard_scored.json"
+                    "scored_model_responses/3D-FRONT_test_pllm_ft-comb/inf_rslts_pllm_ft-comb_3D-FRONT_test_standard_scored.json"
+                    "scored_model_responses/3D-FRONT_test_shpllm_ft-upd/inf_rslts_shapellm-13b-general-v1.0-finetune-upd_ft-upd_3D-FRONT_test_standard_scored.json"
+                    "scored_model_responses/3D-FRONT_test_llava3d_ft-upd_3D-FRONT/inf_rslts_llava3d_ft-upd_3D-FRONT_3D-FRONT_test_standard_scored.json"
+                )
+            else
+                base_model_files+=(
+                    "./scored_model_responses/Crops3D_gpt-5-nano_test_mgpt3d_base/inf_rslts_mgpt3d_base_Crops3D_test_standard_scored.json"
+                    "scored_model_responses/Crops3D_gpt-5-nano_test_gplm_base/inf_rslts_gplm_base_Crops3D_test_standard_scored.json"
+                    "scored_model_responses/Crops3D_gpt-5-nano_test_pllm_base/inf_rslts_pllm_base_Crops3D_test_standard_scored.json"
+                    "scored_model_responses/Crops3D_gpt-5-nano_test_shpllm_ft-cap3d/inf_rslts_shapellm-13b-general-v1.0-finetune_ft-cap3d_Crops3D_test_standard_scored.json"
+                    "scored_model_responses/Crops3D_gpt-5-nano_test_llava3d_base/inf_rslts_llava3d_base_Crops3D_test_standard_scored.json"
+                )
+                
+                trained_model_files+=(
+                    "./scored_model_responses/Crops3D_gpt-5-nano_test_mgpt3d_ft-comb/inf_rslts_mgpt3d_ft-comb_Crops3D_test_standard_scored.json"
+                    "scored_model_responses/Crops3D_gpt-5-nano_test_gplm_ft-comb/inf_rslts_gplm_ft-comb_Crops3D_test_standard_scored.json"
+                    "scored_model_responses/Crops3D_gpt-5-nano_test_pllm_ft-comb/inf_rslts_pllm_ft-comb_Crops3D_test_standard_scored.json"
+                    "scored_model_responses/Crops3D_gpt-5-nano_test_shpllm_ft-upd/inf_rslts_shapellm-13b-general-v1.0-finetune-upd_Crops3D_gpt-5-nano_ft-upd_Crops3D_test_standard_scored.json"
+                    "scored_model_responses/Crops3D_gpt-5-nano_test_llava3d_ft-upd_Crops3D_gpt-5-nano/inf_rslts_llava3d_ft-upd_Crops3D_gpt-5-nano_Crops3D_test_standard_scored.json"
+                )
+            fi
+        fi
+        
+        python analyze_scored_responses_radar_models.py \
+          --title "$formatted_title" \
+          --output_folder_name "$output_folder" \
+          --no_legend \
+          $DUAL_FLAG \
+          --series "Base Model $formatted_title Performance" \
+            "${base_model_files[@]}" \
+          --series "Trained/Finetuned $formatted_title Performance" green \
+            "${trained_model_files[@]}"
+        
+        echo "Completed category: $category for $dataset"
+        echo "---"
+    done
     
-    python analyze_scored_responses_radar_models.py \
-      --title "$formatted_title" \
-      --output_folder_name "$output_folder" \
-      --no_legend \
-      $DUAL_FLAG \
-      --series "Base Model $formatted_title Performance" \
-        "${base_model_files[@]}" \
-      --series "Trained/Finetuned $formatted_title Performance" green \
-        "${trained_model_files[@]}"
-    
-    echo "Completed category: $category"
-    echo "---"
+    echo "Completed all categories for $dataset"
+    echo "========================================"
+    echo ""
 done
 
 echo "All categories processed!"
