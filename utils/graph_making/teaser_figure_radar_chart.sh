8ae4abae850705ddbf597c23cc84fb0924f7116b
Michael E. Elgin <melgin@uwyo.edu>
2025-12-17 15:46:21 -0700

    utils/graph_making/teaser_figure_radar_chart.sh for handy reproducible teaser figure making

---
+++ utils/graph_making/teaser_figure_radar_chart.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+python analyze_scored_responses_radar.py \
+    ./scored_model_responses/3D-FRONT_test_3dllm_base_gpt-4.1-mini \
+    ./scored_model_responses/3D-FRONT_test_mgpt3d_gpt-4.1-mini \
+    ./scored_model_responses/3D-FRONT_test_gplm_gpt-4.1-mini \
+    ./scored_model_responses/3D-FRONT_test_pllm_gpt-4.1-mini \
+    ./scored_model_responses/3D-FRONT_test_shpllm_ft-cap3d_gpt-4.1-mini \
+    ./scored_model_responses/3D-FRONT_test_llava3d_base_gpt-4.1-mini \
+    --legend_names "3D-LLM" "MiniGPT-3D" "GreenPLM" "PointLLM" "ShapeLLM" "LLaVA-3D" \
+    --title "Model Comparison on UPD Tasks" \
+    --no_plot_upd \
+    --legend_bbox_to_anchor 1.3,1.115
